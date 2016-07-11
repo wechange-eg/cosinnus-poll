4a1d87f777ca6cd159ffc63529529bbde2317a5e
Sascha Narr <saschanarr@googlemail.com>
2016-07-11 13:21:39 +0200

    Search results

--- cosinnus_poll/search_indexes.py
+++ cosinnus_poll/search_indexes.py
@@ -10,13 +10,6 @@ from cosinnus_poll.models import Poll
 
 class PollIndex(BaseTaggableObjectIndex, indexes.Indexable):
     description = indexes.CharField(model_attr='description', null=True)
-    location = indexes.CharField(model_attr='location', null=True)
-    street = indexes.CharField(model_attr='street', null=True)
-    zipcode = indexes.CharField(model_attr='zipcode', null=True)
-    city = indexes.CharField(model_attr='city', null=True)
-
-    image = indexes.CharField(model_attr='image', indexed=False, null=True)
-    url = indexes.CharField(model_attr='url', indexed=False, null=True)
 
     def get_model(self):
         return Poll
